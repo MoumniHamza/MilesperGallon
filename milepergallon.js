8a4516954ee7723f13ce1558d959080c1274f0d3
Hamza Moumni <94747@supinfo.com>
2015-09-16 10:30:20 -0700

    Simple console app that displas the mile per gallon you drove during a given time

---
+++ milepergallon.js
@@ -0,0 +1,10 @@
+﻿
+var mile = prompt("How many miles did you drove today?");
+mile = parseFloat(mile);
+alert("You drove:  " + mile);
+var gallon = prompt("How many gallons did you consume today");
+gallon = parseFloat(gallon);
+alert("The number of gallons you used is:  " + gallon);
+var mpg = mile / gallon;
+mpg = parseFloat(mpg);
+alert("You consumed:  " +mpg + "  gallons while you were driving today")
